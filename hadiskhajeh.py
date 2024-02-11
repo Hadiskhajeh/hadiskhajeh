3514ddd3da992db0228172629f1f205167c04538
Hadiskhajeh <159533406+Hadiskhajeh@users.noreply.github.com>
2024-02-11 23:51:48 +0330

    Update hadiskhajeh.py

--- hadiskhajeh.py
+++ hadiskhajeh.py
@@ -1,9 +1,8 @@
->>> n = 4
->>> for i in range(n, 0, -1):
-...     for j in range (i):
-...         print('*', end=' ')
-...     print()
-...
+ n = 4
+for i in range(n, 0, -1):
+  for j in range (i):
+    print('*', end=' ')
+    print()
 * * * *
 * * *
 * *
